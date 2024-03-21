b60b808735cc367b6384c0cf7d8f4ac03ee3c769
Camilo Álvarez <calvarez@consultec-ti.com>
2024-03-21 00:11:23 -0500

    Add message responsive no mobile

--- src/components/templates/MainTemplate.tsx
+++ src/components/templates/MainTemplate.tsx
@@ -3,6 +3,7 @@ import Card from "../atoms/Card";
 import PlanDetails from "../organisms/PlanDetails";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import videoToolLogo from "../../assets/images/dashboard-logo.svg";
 
 interface MainTemplateProps {
   children: React.ReactNode;
@@ -11,7 +12,20 @@ interface MainTemplateProps {
 const MainTemplate = ({ children }: MainTemplateProps) => {
   return (
     <>
-      <div className="fixed top-0 left-0 w-full h-full overflow-auto bg-gray p-5 flex flex-row flex-nowrap gap-5">
+      <div className="h-screen flex flex-col gap-10 justify-center items-center lg:hidden border p-7">
+        <img
+          src={videoToolLogo}
+          alt="video-tool-logo"
+          className="w-3/12 animate-bounce"
+        />
+        <p className="border-purple border-2 rounded-lg px-10 py-5 text-purple text-center">
+          No disponible en versión móvil.
+        </p>
+        <p className="text-center">
+          Be Master Dashboard Video Tool disponible en desktop.
+        </p>
+      </div>
+      <div className="hidden lg:fixed lg:top-0 lg:left-0 lg:w-full lg:h-full lg:overflow-auto lg:bg-gray lg:p-5 lg:flex lg:flex-row lg:flex-nowrap lg:gap-5">
         <div className="w-80">
           <Card color="white" height="h-full" overflow>
             <div className="flex justify-end -mt-5 -me-3">
