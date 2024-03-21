3f8f383ee0925a17366db49e001bc9de96198c11
Camilo Álvarez <calvarez@consultec-ti.com>
2024-03-20 22:59:41 -0500

    Add redirect to videos in folder page

--- src/components/molecules/Breadcrumb.tsx
+++ src/components/molecules/Breadcrumb.tsx
@@ -1,17 +1,20 @@
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import Button from "../atoms/Button";
+import { Link } from "react-router-dom";
 
 const Breadcrumb = () => {
   return (
     <>
       <div className="flex flex-row items-center m-2">
-        <Button
-          title=""
-          color="purple"
-          size="lg"
-          textColor="white"
-          iconName={faArrowLeft}
-        />
+        <Link to="/videos">
+          <Button
+            title=""
+            color="purple"
+            size="lg"
+            textColor="white"
+            iconName={faArrowLeft}
+          />
+        </Link>
         <nav className="ms-3">
           <ol className="flex flex-row gap-4 text-lg">
             <li>Home</li>
